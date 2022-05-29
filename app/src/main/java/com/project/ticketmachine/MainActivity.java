3a587d0618dd5fa43e105300c9d4e4a320261617
thanoskrs <thanoskouros99@gmail.com>
2022-05-30 01:32:39 +0300

    updated MainActivity.java, acticity_main_xml
    added all languages

--- app/src/main/java/com/project/ticketmachine/MainActivity.java
+++ app/src/main/java/com/project/ticketmachine/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,11 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
         ImageButton ticketBtn = (ImageButton) findViewById(R.id.ticketButton);
         ImageButton cardBtn = (ImageButton) findViewById(R.id.cardButton);
-        ImageButton englishBtn = (ImageButton) findViewById(R.id.englishButton);
         ImageButton ticketInfoBtn = (ImageButton) findViewById(R.id.ticketInfoButton);
         ImageButton cardInfoBtn = (ImageButton) findViewById(R.id.cardInfoButton);
 
-        TextView languageText = (TextView) findViewById(R.id.language_text);
+        ImageButton greeceBtn = (ImageButton) findViewById(R.id.greeceButton);
+        ImageButton ukBtn = (ImageButton) findViewById(R.id.ukButton);
+        ImageButton italyBtn = (ImageButton) findViewById(R.id.italyButton);
+        ImageButton germanBtn = (ImageButton) findViewById(R.id.germanButton);
+        ImageButton franceBtn = (ImageButton) findViewById(R.id.franceButton);
+        ImageButton saudiArabiaBtn = (ImageButton) findViewById(R.id.saudiArabiaButton);
+        ImageButton russiaBtn = (ImageButton) findViewById(R.id.russiaButton);
+
+        ImageButton imageButtons[] = {greeceBtn, ukBtn, italyBtn, germanBtn, franceBtn, saudiArabiaBtn, russiaBtn};
+
         TextView ticketRechargeText = (TextView) findViewById(R.id.buy_or_recharge_ticket_text);
         TextView cardRechargeText = (TextView) findViewById(R.id.recharge_card_text);
         TextView ticketInfoText = (TextView) findViewById(R.id.ticketInfoText);
@@ -49,25 +58,72 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        englishBtn.setOnClickListener(new View.OnClickListener() {
+        ukBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (languageText.getText().toString().equals("EN")) {
-                    languageText.setText("GR");
-                    englishBtn.setImageResource(R.drawable.greek);
-                    ticketInfoText.setText("\nPress here if you want to buy a new ticket or recharge existing ticket");
-                    cardInfoText.setText("\nPress here if you want to recharge your card. (Personalized, Anonymous, Unemployed Card)");
-                    ticketRechargeText.setText("Buy or Recharge \nTicket");
-                    cardRechargeText.setText("Recharge \nCard");
-                } else {
-                    languageText.setText("EN");
-                    englishBtn.setImageResource(R.drawable.english);
-                    ticketInfoText.setText("\nΠατήστε εδώ εάν θέλετε να εκδόσετε νέο εισιτήριο ή να επαναφορτίσετε κάποιο υπάρχον εισιτήριο.");
-                    cardInfoText.setText("\nΠατήστε εδώ εάν θέλετε να επαναφορτίσετε την κάρτα σας. (Προσωποποιημένη, Ανώνυμη, Κάρτα Ανέργων)");
-                    ticketRechargeText.setText("Αγορά ή Επαναφόρτιση \nΕισιτηρίου");
-                    cardRechargeText.setText("Επαναφόρτιση \nΚάρτας");
-                }
+                setAlphaForLanguageButtons(imageButtons);
+                ukBtn.setAlpha(1.0f);
+                ticketInfoText.setText("\nPress here if you want to buy a new ticket or recharge existing ticket");
+                cardInfoText.setText("\nPress here if you want to recharge your card. (Personalized, Anonymous, Unemployed Card)");
+                ticketRechargeText.setText("Buy or Recharge \nTicket");
+                cardRechargeText.setText("Recharge \nCard");
+
+            }
+        });
+
+        greeceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                setAlphaForLanguageButtons(imageButtons);
+                greeceBtn.setAlpha(1.0f);
+                ticketInfoText.setText("\nΠατήστε εδώ εάν θέλετε να εκδόσετε νέο εισιτήριο ή να επαναφορτίσετε κάποιο υπάρχον εισιτήριο.");
+                cardInfoText.setText("\nΠατήστε εδώ εάν θέλετε να επαναφορτίσετε την κάρτα σας. (Προσωποποιημένη, Ανώνυμη, Κάρτα Ανέργων)");
+                ticketRechargeText.setText("Αγορά ή Επαναφόρτιση \nΕισιτηρίου");
+                cardRechargeText.setText("Επαναφόρτιση \nΚάρτας");
+
+
+            }
+        });
+
+        italyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAlphaForLanguageButtons(imageButtons);
+                italyBtn.setAlpha(1.0f);
+            }
+        });
+
+        germanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAlphaForLanguageButtons(imageButtons);
+                germanBtn.setAlpha(1.0f);
+            }
+        });
+
+        franceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAlphaForLanguageButtons(imageButtons);
+                franceBtn.setAlpha(1.0f);
+            }
+        });
+
+        saudiArabiaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAlphaForLanguageButtons(imageButtons);
+                saudiArabiaBtn.setAlpha(1.0f);
+            }
+        });
+
+        russiaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAlphaForLanguageButtons(imageButtons);
+                russiaBtn.setAlpha(1.0f);
             }
         });
 
@@ -104,5 +160,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setAlphaForLanguageButtons(ImageButton[] imageButtons) {
+        for (int i = 0; i < imageButtons.length; i++)
+            imageButtons[i].setAlpha(0.5f);
+    }
 
 }
