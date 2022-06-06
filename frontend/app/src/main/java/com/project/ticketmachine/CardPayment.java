da9d0be6a9c4db3c547fd51c28db66b4638709b7
thanoskrs <thanoskouros99@gmail.com>
2022-06-06 18:25:40 +0300

    added CardPayment.java and CashPayment.java with xmls
    added  screens for payment with  the 2 options: by cash or by card.

---
+++ frontend/app/src/main/java/com/project/ticketmachine/CardPayment.java
@@ -0,0 +1,19 @@
+package com.project.ticketmachine;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CardPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
